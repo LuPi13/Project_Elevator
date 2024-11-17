1d7e4caeee470adc33082eedbbe04a5d420958b3
LuPi13 <lupi1313@naver.com>
2024-11-18 02:29:47 +0900

    :tada: WELCOME TO HELL

--- Project_Elevator.srcs/sources_1/new/Project_Elevator.v
+++ Project_Elevator.srcs/sources_1/new/Project_Elevator.v
@@ -4,10 +4,14 @@
 module Project_Elevator(destination, emergency, summon, close, depart, seg_data, seg_sel, door_status, RGB, sound, LCD_E, LCD_RS, LCD_RW, LCD_DATA, rst, clk);
 
     input [6:0] destination;
+    wire [6:0] destination_trig;
     input [1:0] emergency;
     input summon, close;
+    reg summon_trig;
     input [6:0] depart;
     input rst, clk;
+    
+    oneshot_universal #(.WIDTH(8)) osu({destination[6:0], summon}, {destination_trig[6:0], summon_trig}, rst, clk);
     
     output [7:0] seg_data;
     output [7:0] seg_sel;
@@ -16,5 +20,24 @@ module Project_Elevator(destination, emergency, summon, close, depart, seg_data,
     output reg sound;
     output reg LCD_E, LCD_RS, LCD_RW;
     output [7:0] LCD_DATA;
+    
+    reg [15:0] cnt;
+    reg [3:0] ev1_state;
+    reg [3:0] ev2_state;
+    reg [11:0] ev1_control;
+    reg [11:0] ev2_control;
 
+    
+    
+    parameter IDLE = 4'b0000, EMERGENCY = 4'b1111, DOOR_OPEN = 4'b0001, MOVE_TO_DEP = 4'b0010, MOVE_TO_DEST = 4'b0011, DOOR_CLOSING = 4'b0100;
+    
+    always @(negedge rst or posedge clk) begin
+        if(!rst) begin
+            ev1_state <= IDLE;
+            ev2_state <= IDLE;
+            ev1_control <= 12'b0001_1101_0000; //0001: 1층시작, 1101: -(이동중 아님), 0000: 카운터
+            ev2_control <= 12'b0001_1101_0000;
+            cnt <= 0;
+        end
+    end
 endmodule
